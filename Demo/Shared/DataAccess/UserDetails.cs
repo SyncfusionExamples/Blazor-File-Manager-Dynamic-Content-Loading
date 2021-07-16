f67059b8497ad8e89ed553d7f108e91f7ab096a5
Piramanayagam R <piramanayagamr@syncfusioncom>
2021-07-16 13:24:36 +0530

    Sample project is added.

---
+++ Demo/Shared/DataAccess/UserDetails.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Demo.Data
+{
+    public class UserDetails
+    {
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+       
+        public int Id { get; set; }
+        public int UserId { get; set; }
+
+        public string Name { get; set; }
+
+    }
+
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+    }
+}
